b9fff45b28a58162aea92ce104a3798ec28caa9b
M-mboya <michaelkam65@gmail.com>
2025-08-04 10:37:53 +0300

    Add calculator assignment

---
+++ calculator-assignment.py
@@ -0,0 +1,29 @@
+ # MY CALCULATOR! 
+#User inputs
+# input the first number
+num1 = float(input("Enter the first number: "))
+
+#Input the second number
+num2 = float(input("Enter the second number: "))
+
+
+#Compute the sum, difference, product, and quotient.
+
+# Add the two numbers ➕
+sum_result = num1 + num2
+
+# Subtract the second number from the first➖
+difference_result = num1 - num2
+
+# Multiply the two numbers ✖️
+product_result = num1 * num2
+
+# Divide the second number by the first ➗
+quotient_result = num1 / num2
+
+# user results
+print(f"Results of your two numbers:")
+print(f"Sum: {sum_result}")  # ➕
+print(f"Difference: {difference_result}")  # ➖
+print(f"Product: {product_result}")  # ✖️
+print(f"Quotient: {quotient_result}")  # ➗
